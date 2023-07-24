687e7580db500524a3e616f89f3606092e7b4f8a
Yash Guard <yashguard>
2023-07-24 23:12:18 +0530

    Ninth query solved

--- index.js
+++ index.js
@@ -103,35 +103,53 @@ server.post("/student", async (req, res) => {
   //   console.log(students);
   // <================== Seventh Query ==================>
   // <================== Bottom 5 students in grade I based on science score ==================>
-//   const studentsList = await student
-//     .find({ class: "I" })
-//     .sort({ science: 1 })
-//     .limit(5);
-//   const students = await student
-//     .find({ class: "I" })
-//     .sort({ science: 1 })
-//     .limit(5)
-//     .count();
-//   res.status(200).send(studentsList);
-//   console.log(students);
+  //   const studentsList = await student
+  //     .find({ class: "I" })
+  //     .sort({ science: 1 })
+  //     .limit(5);
+  //   const students = await student
+  //     .find({ class: "I" })
+  //     .sort({ science: 1 })
+  //     .limit(5)
+  //     .count();
+  //   res.status(200).send(studentsList);
+  //   console.log(students);
   // <================== Eighth Query ==================>
   // <================== Students from section A who scored less than 50 in all the subjects ==================>
-    const studentsList = await student.find({
-      section: "A",
-      maths: { $lt: 50 },
-      science: { $lt: 50 },
-      english: { $lt: 50 },
-    });
-    const students = await student
-      .find({
-        section: "A",
-        maths: { $lt: 50 },
-        science: { $lt: 50 },
-        english: { $lt: 50 },
-      })
-      .count();
-    res.status(200).send(studentsList);
-    console.log(students);
+  // const studentsList = await student.find({
+  //   section: "A",
+  //   maths: { $lt: 50 },
+  //   science: { $lt: 50 },
+  //   english: { $lt: 50 },
+  // });
+  // const students = await student
+  //   .find({
+  //     section: "A",
+  //     maths: { $lt: 50 },
+  //     science: { $lt: 50 },
+  //     english: { $lt: 50 },
+  //   })
+  //   .count();
+  // res.status(200).send(studentsList);
+  // console.log(students);
+  // <================== Ninth Query ==================>
+  // <================== Students from section C who scored more than 75 in all the subjects ==================>
+  const studentsList = await student.find({
+    section: "C",
+    maths: { $gt: 75 },
+    science: { $gt: 75 },
+    english: { $gt: 75 },
+  });
+  const students = await student
+    .find({
+      section: "C",
+      maths: { $gt: 75 },
+      science: { $gt: 75 },
+      english: { $gt: 75 },
+    })
+    .count();
+  res.status(200).send(studentsList);
+  console.log(students);
 });
 
 server.listen(8010, () => {
